16eb7d4e271a428f7cd4f28366012439f4deef81
cforkus <114452879+cforkus@users.noreply.github.com>
2023-04-11 17:29:59 -0400

    Update capturingimages.py

--- capturingimages.py
+++ capturingimages.py
@@ -1,6 +1,7 @@
 import picamera
 import time
 import os
+import subprocess
 
 #initialize the camera
 camera = picamera.PiCamera()
@@ -14,6 +15,7 @@ filename = 'image'
 # specify the destination directory
 destination = "/home/pi/ndviphotos/{}.jpg".format(filename)
 
+try:
 # wait for a key press to capture the image
 input('Press Enter to capture the image...')
 
@@ -29,6 +31,24 @@ time.sleep(2)
 #close the camera
 camera.close()
 
+# check if feh is installed, and install it if not
+feh_check = subprocess.run(['which', 'feh'], capture_output=True, text=True)
+if feh_check.returncode != 0:
+   print('feh is not installed, installing...')
+   subprocess.run(['sudo', 'apt-get', 'update'])
+   subprocess.run(['sudo', 'apt-get', 'install', '-y', 'feh'])
+   print('feh installation complete')
+
 #open in default image viewer
 os.system('feh ' + destination)
 
+except KeyboardInterrupt:
+    # If user interrupts the program with Ctrl+C
+    print('\nProgram interrupted by user')
+except Exception as e:
+    # If an error occurs
+    print('An error occurred:', str(e))
+
+finally:
+    # always close the camera, even if an error occurred
+    camera.close()
