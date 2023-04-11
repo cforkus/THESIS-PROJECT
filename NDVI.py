441f4596d8184a41390b1adc3c25b6aa056605cc
cforkus <114452879+cforkus@users.noreply.github.com>
2023-04-11 17:23:01 -0400

    Update NDVI.py

--- NDVI.py
+++ NDVI.py
@@ -4,40 +4,37 @@ import numpy as np
 import os
 import RPi.GPIO as GPIO
 
+# Define paths
+photo_path = "/home/pi/ndviphotos/"
 timestamp = strftime("%d:%m:%y:%I:%M:%S:%p")
+noir_path = "{}{}.jpg".format(photo_path, timestamp + "Noir")
+rbg_path = "{}{}.jpg".format(photo_path, timestamp + "RBG")
+ndvi_path = "{}{}.jpg".format(photo_path, timestamp + "ndvi")
 
 # Set GPIO pin outputs
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(7,GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 
-# Get Noir and RBG images
+# Take Noir photo
+GPIO.output(7, GPIO.LOW)
+os.system("raspistill -o {}".format(noir_path))
 
-# Take Noir
-GPIO. output(7,GPIO.LOW)
-os.system('raspistill -o /home/pi/ndviphotos/{}.jpg'. format(timestamp + "NOIR"))
+# Take RBG photo
+GPIO.output(7, GPIO.HIGH)
+os.system("raspistill -o {}".format(rbg_path))
 
-# Take RBG
-GPIO. output(7,GPIO.HIGH)
-os.system('raspistill -o /home/pi/ndviphotos/{}.jpg'. format(timestamp + "RBG"))
+# Increase brightness and contrast of Noir photo
+convert_command = "convert -brightness-contrast -20x30 {} {}".format(noir_path, noir_path)
+os.system(convert_command)
 
-image_path_Noir = ("/home/pi/ndviphotos/{}.jpg").format(timestamp + "Noir")
-image_path_RBG = ("/home/pi/ndviphotos/{}.jpg").format(timestamp + "RBG")
+# Convert Noir and RBG photos into arrays
+with cv2.imread(noir_path) as image:
+    image = np.array(image, dtype=float) / float(255)
+with cv2.imread(rbg_path) as image2:
+    image2 = np.array(image2, dtype=float) / float(255)
 
-#increase brightness and contrast of image
-string_convert = ("convert -brightness-contrast -20x30" + "" + image_path_Noir + "" + image_path_Noir)
-os.system(string_convert)
-
-#convert Noir photo into array
-image = cv2.imread(image_path_Noir)
-image = np.array(image,dtype=float)/float(255)
-
-#convert RBG photo into array
-image2 = cv2.imread(image_path_RBG)
-image2 = np.array(image2,dtype=float)/float(255)
-
-#get NIR values from Noir photo
+# Get NIR values from Noir photo
 def get_nir(image):
     if image.ndim == 3:
         b, g, r = cv2.split(image)
@@ -46,23 +43,19 @@ def get_nir(image):
         nir = image.astype(float)
     return nir
 
-#print(get_nir(image))
-#cv2.imwrite('{}.jpg").format(timestamp + "Noir_red"), get_nir(image))
-
-#get red value from RBG
+# Get red value from RBG photo
 def get_red(image2):
     b, g, r, = cv2.split(image2)
     red = r.astype(float)
     return red
 
-#print(get_red(image2))
-#cv2.imwrite('/home/pi/ndviphotos/{}.jpg").format(timestamp + "RBG_red"), get_red(image2))
-
+# Calculate NDVI and save output
 def ndvi(image, image2):
     bottom = (get_nir(image) + get_red(image2))
-    bottom[bottom==0] = 0.01
+    bottom[bottom == 0] = 0.01
     ndvi = (get_nir(image) - get_red(image2)) / bottom
     ndvi = np.multiply(ndvi, 1000)
-    return ndvi
+    cv2.imwrite(ndvi_path, ndvi)
 
-cv2.imwrite('/home/pi/ndviphotos/{}.jpg'.format(timestamp + "ndvi"), ndvi(image, image2))
+ndvi(image, image2)
+
