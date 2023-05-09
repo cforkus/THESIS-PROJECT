a1f10e92d4ce9f185798bfed0b27c1724941eb46
cforkus <114452879+cforkus@users.noreply.github.com>
2023-05-09 15:14:10 -0400

    Update capturingimages.py

--- capturingimages.py
+++ capturingimages.py
@@ -2,9 +2,8 @@ import picamera
 import time
 import os
 import subprocess
-import cv2
 
-# Initialize the camera
+#initialize the camera
 camera = picamera.PiCamera()
 
 # Disable camera effects and image processing
@@ -13,53 +12,49 @@ camera.color_effects = None
 camera.rotation = 0
 camera.awb_mode = 'off'
 camera.exposure_mode = 'auto'
-# Set the resolution of the camera
+#set the resolution of camera
 camera.resolution = (640, 480)
 
-# Set filename for the image
+#set filename for the image
 filename = 'image'
 
-# Specify the destination directory
-destination = "/home/pi/ndviphotos/{}.raw".format(filename)
+# specify the destination directory
+destination = "/home/pi/ndviphotos/{}.jpg".format(filename)
 
 try:
-    # Wait for a key press to capture the image
-    input('Press Enter to capture the image...')
+   # wait for a key press to capture the image
+   input('Press Enter to capture the image...')
 
-    # Capture the raw Bayer image
-    camera.capture(destination, format='raw')
+   #capture the image
+   camera.capture(destination)
 
-    # Print a message that the image has been captured
-    print('Image captured successfully!')
+   # print a message that image has been captures
+   print('image captured successfully!')
 
-    # Perform demosaicing to obtain a full-color image
-    raw_image = cv2.imread(destination, cv2.IMREAD_UNCHANGED)
-    color_image = cv2.cvtColor(raw_image, cv2.COLOR_BayerRG2RGB)
+   #wait for 2 seconds before exiting
+   time.sleep(2)
 
-    # Specify the output filename for the color image
-    color_image_destination = "/home/pi/ndviphotos/{}.jpg".format(filename)
+   #close the camera
+   camera.close()
 
-    # Save the demosaiced color image
-    cv2.imwrite(color_image_destination, color_image)
+   # check if feh is installed, and install it if not
+   feh_check = subprocess.run(['which', 'feh'], capture_output=True, text=True)
+   if feh_check.returncode != 0:
+      print('feh is not installed, installing...')
+      subprocess.run(['sudo', 'apt-get', 'update'])
+      subprocess.run(['sudo', 'apt-get', 'install', '-y', 'feh'])
+      print('feh installation complete')
 
-    # Check if feh is installed, and install it if not
-    feh_check = subprocess.run(['which', 'feh'], capture_output=True, text=True)
-    if feh_check.returncode != 0:
-        print('feh is not installed, installing...')
-        subprocess.run(['sudo', 'apt-get', 'update'])
-        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'feh'])
-        print('feh installation complete')
-
-    # Open the color image in the default image viewer
-    os.system('feh ' + color_image_destination)
+   #open in default image viewer
+   os.system('feh ' + destination)
 
 except KeyboardInterrupt:
-    # If the user interrupts the program with Ctrl+C
-    print('\nProgram interrupted by the user')
+    # If user interrupts the program with Ctrl+C
+    print('\nProgram interrupted by user')
 except Exception as e:
     # If an error occurs
     print('An error occurred:', str(e))
 
 finally:
-    # Always close the camera, even if an error occurred
+    # always close the camera, even if an error occurred
     camera.close()
